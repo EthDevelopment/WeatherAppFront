df25418b2b9db4ec827cd23d4bf054e99c2aac91
Swaleh Abdulrehman <swaleh.abdulrehman@ihsmarkit.com>
2023-12-08 11:09:55 +0000

    search bar added onto hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,16 @@
 // hero.js
 import React from 'react';
 import './Hero.css'; // Import your CSS for styling
+import SearchBar from '../SearchBar/SearchBar';
 
 // hero.js
 const Hero = () => {
     return (
       <div className="hero-container">
         <div className="hero-image" style={{ backgroundImage: 'url(../assets/ice.jpeg)' }}>
-          <h1>Your Hero Title</h1>
+          <h1>Weather Tracker</h1>
           <p>Your hero description goes here.</p>
+          <SearchBar/>
         </div>
       </div>
     );
