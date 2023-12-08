cb19af9b57a9373c4df4a276dc554a5e4e4e20b4
Swaleh Abdulrehman <swaleh.abdulrehman@ihsmarkit.com>
2023-12-08 00:11:13 +0000

    updated file structure preparing for day night toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+// App.js
+// IMPORTS
+import React, { useState } from 'react';
+// Page Overlays
+import Navbar from './components/Nav/Navbar';
+import DayNightToggle from './components/DayNight/daynighttoggle'; // Import the DayNightToggle component
+// Components/pages
+import Hero from './components/Hero/Hero';
+// CSS
 import './App.css';
 
+// Function to toggle the theme (defined at the top level of the module)
+export const toggleTheme = (theme, setTheme) => {
+  // Toggle between 'light' and 'dark' themes
+  setTheme(theme === 'light' ? 'dark' : 'light');
+};
+
 function App() {
+  // State to manage the theme ('light' or 'dark')
+  const [theme, setTheme] = useState('dark');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      {/* SocialIconsBar component to display the vertical bar with social media icons */}
+      <Navbar />
+      {/* Your other content */}
+      <section id="Home">
+        <Hero theme={theme} setTheme={setTheme} /> {/* Pass theme and setTheme as props */}
+      </section> 
     </div>
   );
 }
 
 export default App;
+export { DayNightToggle };
